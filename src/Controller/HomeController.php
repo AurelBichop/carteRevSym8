8a998bc7a868fd62a662df6e5057d4f877e4e900
AurelBichop <linux.aurelien@gmail.com>
2026-08-20 16:24:38 +0200

    add style with Bootstrap5

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(Filesystem $filesystem): Response
+    {
+        //Recuperer les fichier .md
+        $phpMd = $filesystem->readFile('../filesCards/php.md');
+        
+        //split chaque contenue de fichier par ***
+        $splitPhpMd = explode('***', $phpMd);
+        
+        //Tirage au sort
+        $choiceCard = array_rand($splitPhpMd);
+ 
+        return $this->render('home/index.html.twig', [
+            'card' => $splitPhpMd[$choiceCard],
+        ]);
+    }
+}
