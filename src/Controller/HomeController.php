dbfdc0c90c91daf0a83d55832ebe1a981d1c2f40
AurelBichop <linux.aurelien@gmail.com>
2026-08-26 20:57:38 +0200

    add md link

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,20 +3,19 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(Filesystem $filesystem): Response
+    public function index(): Response
     {
         //Créer un service pour la logique ci dessous
 
         //Recuperer les fichier .md
-        $phpMd = $filesystem->readFile('../filesCards/php.md');
-        
+        $phpMd = file_get_contents($this->getParameter('symfony.md.file'));
+
         //split chaque contenue de fichier par ***
         $separator = $this->getParameter('separator.string');
         $splitPhpMd = explode($separator, $phpMd);
